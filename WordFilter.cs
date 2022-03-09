8d58d6ae65c6ede624e76c95aac5ecba46268bd2
Tom Barrett <barrett.thomas@mayo.edu>
2022-03-09 05:52:26 -0600

    Adding static method for word filtering in order to create unit tests

---
+++ WordFilter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordleAssist {
+    public static class WordFilter {
+        public static List<string> GetValidWords(List<string> ListOfWords, char[] grayLetters, List<char[]> yellowFields, List<string> greenLetters) {
+            throw new NotImplementedException();
+        }
+    }
+}
